33732fc42d88645cf7341474ff4ee7f1307f1053
Ivan Petrov <ivanp6174@gmail.com>
2026-05-17 16:08:43 +0800

    imrove: winit 0.30.13

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,57 @@
-use winit::dpi::PhysicalSize;
-use winit::event::{Event, WindowEvent};
-use winit::event_loop::{ControlFlow, EventLoop};
-use winit::window::{Window, WindowBuilder};
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        self.window = Some(event_loop.create_window(Window::default_attributes()).unwrap());
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            },
+            WindowEvent::RedrawRequested => {
+                // Redraw the application.
+                //
+                // It's preferable for applications that do not render continuously to render in
+                // this event rather than in AboutToWait, since rendering in here allows
+                // the program to gracefully handle redraws requested by the OS.
+
+                // Draw.
+
+                // Queue a RedrawRequested event.
+                //
+                // You only need to call this if you've determined that you need to redraw in
+                // applications which do not always need to. Applications that redraw continuously
+                // can render here instead.
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
 
 fn main() {
-    // Создаем цикл обработки событий?
     let event_loop = EventLoop::new().unwrap();
 
-    // Что за метод into
-    let window_size: PhysicalSize<u32> = (800, 600).into();
+    // ControlFlow::Poll continuously runs the event loop, even if the OS hasn't
+    // dispatched any events. This is ideal for games and similar applications.
+    event_loop.set_control_flow(ControlFlow::Poll);
 
-    // Создаем окно задав его параметры
-    let window = WindowBuilder::new()
-        .with_fullscreen(None)
-        .with_inner_size(window_size)
-        .with_title("wgpu first steps")
-        .build(&event_loop)
-        .unwrap();
+    // ControlFlow::Wait pauses the event loop if no events are available to process.
+    // This is ideal for non-game applications that only update in response to user
+    // input, and uses significantly less power/CPU time than ControlFlow::Poll.
+    event_loop.set_control_flow(ControlFlow::Wait);
 
-    // Запустим цикл обработки событий, передав в него замыкание,
-    //которое будет выполняться на каждой итерации цикла
-    event_loop.run(move |event, elwt| {
-        // будем попадать в тело цикла только при появлении события ос
-        match event {
-            // если было запрошено событие окна, завершаем цикл
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                elwt.exit();
-            } 
-
-            // Остальные события нам не интересны
-            _ => {}
-        }
-    });;
+    let mut app = App::default();
+    event_loop.run_app(&mut app);
 }
